0dae405386098dc1bf6d44d1e59ccdf0f10807d5
Andrés <carlosraxon019@gmail.com>
2026-05-13 16:56:57 -0600

    feat(api): handle enviroment variables with zod

---
+++ apps/api/src/config/env.ts
@@ -0,0 +1,25 @@
+/** biome-ignore-all lint/performance/noNamespaceImport: zod best approach */
+
+import { NODE_ENV } from '@zentro/constants/env'
+import { config } from 'dotenv'
+import * as z from 'zod'
+
+config()
+
+const envSchema = z.object({
+  PORT: z.coerce.number(),
+  BASE_URL: z.url(),
+  DEV_IP: z.ipv4(),
+  DATABASE_URL: z.url(),
+  WEB_URL: z.url(),
+  NODE_ENV: z.enum(Object.values(NODE_ENV)),
+  DATABASE_URL_TEST: z.url(),
+  BETTER_AUTH_SECRET: z.string(),
+  BETTER_AUTH_URL: z.url(),
+  GITHUB_CLIENT_ID: z.string(),
+  GITHUB_CLIENT_SECRET: z.string(),
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+})
+
+export const env = envSchema.parse(process.env)

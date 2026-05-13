8381148eb234892d1883fb171f93008ad6467072
Andrés <carlosraxon019@gmail.com>
2026-05-13 17:41:00 -0600

    feat: add notes schema validation

---
+++ packages/schemas/src/notes.ts
@@ -0,0 +1,21 @@
+import { noteBackgroundColors } from '@zentro/constants/notes'
+import { z } from 'zod'
+
+const invalidType = 'Invalid type'
+
+export const createNoteSchema = z.object({
+  title: z
+    .string(invalidType)
+    .trim()
+    .min(1, { error: 'Add the note title' })
+    .max(100, { error: 'Max of 100 characters' }),
+  content: z.string(invalidType).trim().max(500, { error: 'Max of 500 characters' }).optional(),
+  color: z.enum(noteBackgroundColors, { error: 'Invalid note color' }),
+  positionX: z.number(invalidType),
+  positionY: z.number(invalidType),
+})
+
+export const updateNoteSchema = createNoteSchema.partial()
+
+export type CreateNoteInput = z.infer<typeof createNoteSchema>
+export type UpdateNoteInput = z.infer<typeof updateNoteSchema>

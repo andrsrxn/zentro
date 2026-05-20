cc17ab19c989d9f67a5c5cfade97fe04df374216
Andrés <carlosraxon019@gmail.com>
2026-05-19 18:41:45 -0600

    feat(web): add handwritten font, scrollbar width and remove selection background

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,11 +1,13 @@
-import { Figtree, Geist_Mono } from 'next/font/google'
+import { Figtree, Geist_Mono, Shadows_Into_Light_Two } from 'next/font/google'
 import type { ReactNode } from 'react'
 import { baseMetadata, baseViewport } from '@/lib/constants/metadata'
 import { cn } from '@/lib/utils/theme'
 import './globals.css'
+import { preconnect } from 'react-dom'
 import { AppProviders } from '@/components/providers/providers'
 import { HotKeys } from '@/components/shared/hot-keys'
 import { Toaster } from '@/components/ui/sonner'
+import { envClient } from '@/lib/config/env-client'
 
 const fontSans = Figtree({
   subsets: ['latin'],
@@ -13,6 +15,14 @@ const fontSans = Figtree({
   style: 'normal',
   weight: 'variable',
   variable: '--font-sans',
+})
+
+const fontHandwritten = Shadows_Into_Light_Two({
+  subsets: ['latin'],
+  display: 'swap',
+  style: 'normal',
+  weight: '400',
+  variable: '--font-handwritten',
 })
 
 const fontMono = Geist_Mono({
@@ -30,8 +40,15 @@ export default function RootLayout({
 }: Readonly<{
   children: ReactNode
 }>) {
+  preconnect(envClient.NEXT_PUBLIC_API_URL, {
+    crossOrigin: 'use-credentials',
+  })
+
   return (
-    <html lang='en' suppressHydrationWarning className={cn(fontMono.variable, fontSans.variable)}>
+    <html
+      lang='en'
+      suppressHydrationWarning
+      className={cn(fontMono.variable, fontHandwritten.variable, fontSans.variable)}>
       <body>
         <AppProviders>
           <main className='flex w-full'>{children}</main>

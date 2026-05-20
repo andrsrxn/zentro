c1ce4c28b373b48a7c6a584956311c75f62030d6
Andrés <carlosraxon019@gmail.com>
2026-05-19 18:43:47 -0600

    feat(web): optimistic creation of note

--- apps/web/components/notes/create-form-dialog.tsx
+++ apps/web/components/notes/create-form-dialog.tsx
@@ -1,5 +1,7 @@
 'use client'
 
+import { NOTES, type NoteBackgroundColor } from '@zentro/constants/notes'
+import { useState } from 'react'
 import { CreateNoteForm } from '@/components/notes/create-form'
 import {
   Dialog,
@@ -9,23 +11,41 @@ import {
   DialogTitle,
 } from '@/components/ui/dialog'
 import { useNotesStore } from '@/lib/store/notes'
+import { getNoteForegroundColor } from '@/lib/utils/notes'
 
 export const CreateNoteFormDialog = () => {
+  const [color, setColor] = useState<NoteBackgroundColor>(NOTES.defaultNoteColor.background)
   const openCreateForm = useNotesStore(state => state.openCreateForm)
   const setOpenCreateForm = useNotesStore(state => state.setOpenCreateForm)
 
   return (
-    <Dialog open={openCreateForm} onOpenChange={setOpenCreateForm}>
-      <DialogContent className='sm:max-w-sm'>
+    <Dialog
+      open={openCreateForm}
+      onOpenChange={open => {
+        if (!open) {
+          setColor(NOTES.defaultNoteColor.background)
+        }
+        setOpenCreateForm(open)
+      }}>
+      <DialogContent className='rounded-xs pt-3 sm:max-w-sm' style={{ backgroundColor: color }}>
         <DialogHeader>
-          <DialogTitle>Create Note</DialogTitle>
+          <DialogTitle className='sr-only' style={{ color: getNoteForegroundColor(color) }}>
+            Create Note
+          </DialogTitle>
           <DialogDescription className='sr-only'>
             Select a color and enter the note content
           </DialogDescription>
         </DialogHeader>
         <CreateNoteForm
-          onSuccess={() => setOpenCreateForm(false)}
-          onCancel={() => setOpenCreateForm(false)}
+          onSuccess={() => {
+            setColor(NOTES.defaultNoteColor.background)
+            setOpenCreateForm(false)
+          }}
+          onCancel={() => {
+            setColor(NOTES.defaultNoteColor.background)
+            setOpenCreateForm(false)
+          }}
+          onColorChange={setColor}
         />
       </DialogContent>
     </Dialog>
--- apps/web/components/notes/create-form.tsx
+++ apps/web/components/notes/create-form.tsx
@@ -1,32 +1,37 @@
 'use client'
 
 import { zodResolver } from '@hookform/resolvers/zod'
-import { NOTES, noteBackgroundColors } from '@zentro/constants/notes'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import {
+  NOTES,
+  type Note,
+  type NoteBackgroundColor,
+  noteBackgroundColors,
+} from '@zentro/constants/notes'
 import { type CreateNoteInput, createNoteSchema } from '@zentro/schemas/notes'
-import type { ComponentProps } from 'react'
+import { type ComponentProps, useState } from 'react'
 import { Controller, useForm } from 'react-hook-form'
 import { toast } from 'sonner'
 import { Button } from '@/components/ui/button'
 import { Field, FieldError, FieldGroup, FieldLabel } from '@/components/ui/field'
 import { Input } from '@/components/ui/input'
-import {
-  InputGroup,
-  InputGroupAddon,
-  InputGroupText,
-  InputGroupTextarea,
-} from '@/components/ui/input-group'
 import { Label } from '@/components/ui/label'
 import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
-import { useCreateNote } from '@/lib/hooks/use-notes'
+import { Textarea } from '@/components/ui/textarea'
+import { createNote } from '@/lib/mutations/notes'
+import { authClient } from '@/lib/services/auth-client'
+import { getNoteForegroundColor } from '@/lib/utils/notes'
 import { cn } from '@/lib/utils/theme'
 
+// biome-ignore lint/complexity/noExcessiveLinesPerFunction: temporal
 export function CreateNoteForm({
   onSuccess,
   onCancel,
+  onColorChange,
   defaultValues = {
-    title: 'Reminder',
+    title: '',
     content: '',
-    color: NOTES.colors.yellow.background,
+    color: NOTES.defaultNoteColor.background,
     positionX: 0,
     positionY: 0,
   },
@@ -34,25 +39,113 @@ export function CreateNoteForm({
 }: {
   onSuccess?: () => void
   onCancel?: () => void
+  onColorChange?: (color: NoteBackgroundColor) => void
   defaultValues?: CreateNoteInput
 } & ComponentProps<'form'>) {
-  const { createNote, isPending, error } = useCreateNote()
-
   const form = useForm({
     resolver: zodResolver(createNoteSchema),
     defaultValues,
   })
+  const [color, setColor] = useState<NoteBackgroundColor>(defaultValues.color)
+  const fgColor = getNoteForegroundColor(color)
+  const session = authClient.useSession()
+  const queryClient = useQueryClient()
 
-  if (error) {
-    toast.error(error.message)
-  }
+  const { mutate } = useMutation({
+    mutationFn: (input: CreateNoteInput) => createNote({ input }),
+
+    onMutate: async newNoteInput => {
+      await queryClient.cancelQueries({
+        queryKey: NOTES.tags.all(),
+      })
+
+      const previousNotes = queryClient.getQueryData<Note[]>(NOTES.tags.all())
+
+      // temporary optimistic note
+      const optimisticNote: Note = {
+        id: '',
+        positionX: 0,
+        positionY: 0,
+        userId: session.data?.user.id ?? crypto.randomUUID(),
+        order: ((previousNotes?.length ?? 0) + 1) * NOTES.orderStep,
+        title: newNoteInput.title,
+        content: newNoteInput.content,
+        color: newNoteInput.color,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      }
+
+      queryClient.setQueryData<Note[]>(NOTES.tags.all(), old =>
+        old ? [...old, optimisticNote] : [optimisticNote]
+      )
+
+      return {
+        previousNotes,
+        // to replace the optimistic note with the real server note
+        optimisticId: optimisticNote.id,
+        optimisticNote,
+      }
+    },
+
+    onSuccess: (createdNote, _variables, context) => {
+      if (!(createdNote && context)) {
+        return
+      }
+
+      const normalizedNote: Note = {
+        ...createdNote,
+        createdAt: new Date(createdNote.createdAt),
+        updatedAt: new Date(createdNote.updatedAt),
+      }
+
+      // replace optimistic note with real server note
+      queryClient.setQueryData<Note[]>(NOTES.tags.all(), old => {
+        if (!old) {
+          return [normalizedNote]
+        }
+
+        return old.map(note => (note.id === context.optimisticId ? normalizedNote : note))
+      })
+
+      queryClient.setQueryData(NOTES.tags.single(normalizedNote.id), normalizedNote)
+
+      const noteElement = document.getElementById(normalizedNote.id)
+      if (noteElement) {
+        noteElement.scrollIntoView({
+          behavior: 'smooth',
+          block: 'center',
+          inline: 'nearest',
+        })
+      }
+      toast.success(NOTES.success.created.message)
+    },
+
+    onError: (error, _variables, context) => {
+      if (context?.previousNotes) {
+        queryClient.setQueryData<Note[]>(NOTES.tags.all(), () =>
+          context?.previousNotes?.toSorted((a, b) => a.order - b.order)
+        )
+      }
+
+      toast.error(error.message)
+    },
+  })
 
   function onSubmit(data: CreateNoteInput) {
-    createNote(data)
-
-    toast.success(NOTES.success.created.message)
     form.reset()
     onSuccess?.()
+
+    mutate(data)
+  }
+
+  function handleColorChange(color: NoteBackgroundColor) {
+    onColorChange?.(color)
+    setColor(color)
+  }
+
+  function handleOnCancel() {
+    form.reset()
+    onCancel?.()
   }
 
   return (
@@ -63,15 +156,43 @@ export function CreateNoteForm({
           control={form.control}
           render={({ field, fieldState }) => (
             <Field data-invalid={fieldState.invalid}>
-              <FieldLabel htmlFor='create-note-title-form'>Title</FieldLabel>
+              <FieldLabel htmlFor='create-note-title-form' className='sr-only'>
+                Title
+              </FieldLabel>
               <Input
                 {...field}
-                disabled={isPending}
+                className='font-handwritten border-none bg-transparent p-0 px-2 text-2xl! focus-visible:ring-0 dark:placeholder:text-neutral-500'
                 id='create-note-title-form'
                 aria-invalid={fieldState.invalid}
-                placeholder='Reminder...'
+                style={{ color: fgColor }}
+                placeholder='Note title...'
                 autoComplete='off'
               />
+              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
+            </Field>
+          )}
+        />
+
+        <Controller
+          name='content'
+          control={form.control}
+          render={({ field, fieldState }) => (
+            <Field data-invalid={fieldState.invalid}>
+              <FieldLabel htmlFor='create-note-content-form' className='sr-only'>
+                Additional details <span className='text-muted-foreground'>(optional)</span>
+              </FieldLabel>
+
+              <Textarea
+                {...field}
+                style={{ color: fgColor }}
+                className='max-h-32 min-h-24 resize-none border-none p-0 px-2 text-base focus-visible:ring-0 dark:placeholder:text-neutral-500'
+                id='create-note-content-form'
+                placeholder='Additional details... (optional)'
+                rows={6}
+                maxLength={500}
+                aria-invalid={fieldState.invalid}
+              />
+
               {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
             </Field>
           )}
@@ -81,18 +202,22 @@ export function CreateNoteForm({
           control={form.control}
           render={({ field, fieldState }) => (
             <Field data-invalid={fieldState.invalid}>
-              <FieldLabel htmlFor='create-note-color-form'>Color</FieldLabel>
+              <FieldLabel htmlFor='create-note-color-form' className='sr-only'>
+                Color
+              </FieldLabel>
               <RadioGroup
                 {...field}
-                disabled={isPending}
-                onValueChange={field.onChange}
+                onValueChange={(value: NoteBackgroundColor) => {
+                  field.onChange(value)
+                  handleColorChange(value)
+                }}
                 className='flex justify-between gap-2'>
                 {noteBackgroundColors.map((color, index) => (
                   <div key={color}>
                     <Label
                       className={cn(
                         'has-focus-within:ring-primary/40 size-8 cursor-pointer rounded-full border transition has-focus-within:ring-3',
-                        field.value === color && 'border-primary border-2'
+                        field.value === color && 'border-primary ring-primary/40 border-2 ring-3'
                       )}
                       style={{ backgroundColor: color }}>
                       <RadioGroupItem className='opacity-0!' id={`color-${index}`} value={color} />
@@ -105,52 +230,13 @@ export function CreateNoteForm({
             </Field>
           )}
         />
-        <Controller
-          name='content'
-          control={form.control}
-          render={({ field, fieldState }) => (
-            <Field data-invalid={fieldState.invalid}>
-              <FieldLabel htmlFor='create-note-content-form'>
-                Content <span className='text-muted-foreground'>(optional)</span>
-              </FieldLabel>
-              <InputGroup>
-                <InputGroupTextarea
-                  {...field}
-                  disabled={isPending}
-                  id='create-note-content-form'
-                  placeholder='Always start with positive thoughts...'
-                  rows={6}
-                  maxLength={500}
-                  className='min-h-24 resize-none'
-                  aria-invalid={fieldState.invalid}
-                />
-                <InputGroupAddon align='block-end'>
-                  <InputGroupText className='tabular-nums'>
-                    {field.value?.length ?? 0}/500 characters
-                  </InputGroupText>
-                </InputGroupAddon>
-              </InputGroup>
-
-              {fieldState.invalid && <FieldError errors={[fieldState.error]} />}
-            </Field>
-          )}
-        />
         <Field orientation='horizontal' className='justify-end'>
           {onCancel && (
-            <Button
-              type='button'
-              variant='outline'
-              disabled={isPending}
-              onClick={() => {
-                form.reset()
-                onCancel()
-              }}>
+            <Button type='button' variant='outline' onClick={handleOnCancel}>
               Cancel
             </Button>
           )}
-          <Button type='submit' disabled={isPending}>
-            {isPending ? 'Creating...' : 'Create Note'}
-          </Button>
+          <Button type='submit'>Create Note</Button>
         </Field>
       </FieldGroup>
     </form>

c5dee306046b7d28a607db061999e53bddf68e68
Andrés <carlosraxon019@gmail.com>
2026-05-27 18:18:59 -0600

    feat(web): add safe error message on login instead of throwing

--- apps/web/lib/utils/auth.ts
+++ apps/web/lib/utils/auth.ts
@@ -1,11 +1,17 @@
 import type { AuthProvider } from '@zentro/constants/auth'
+import { HTTP_ERRORS } from '@zentro/constants/errors'
+import { toast } from 'sonner'
 import { envClient } from '@/lib/config/env-client'
 import { authClient } from '@/lib/services/auth-client'
 
 export const signInWith = async (provider: AuthProvider) => {
-  await authClient.signIn.social({
-    provider,
-    callbackURL: envClient.NEXT_PUBLIC_BASE_URL,
-    errorCallbackURL: envClient.NEXT_PUBLIC_BASE_URL,
-  })
+  try {
+    await authClient.signIn.social({
+      provider,
+      callbackURL: envClient.NEXT_PUBLIC_BASE_URL,
+      errorCallbackURL: envClient.NEXT_PUBLIC_BASE_URL,
+    })
+  } catch {
+    toast.error(HTTP_ERRORS.internalError.message)
+  }
 }

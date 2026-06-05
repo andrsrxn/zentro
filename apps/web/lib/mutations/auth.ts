8c941a83ec64090e0f4debd77aab85c4c843f26c
Andrés <carlosraxon019@gmail.com>
2026-06-05 17:30:16 -0600

    refactor(web): centralize auth functions on mutations instead on utils

--- apps/web/lib/mutations/auth.ts
+++ apps/web/lib/mutations/auth.ts
@@ -1,18 +1,62 @@
-import { HTTP_ERRORS } from '@zentro/constants/errors'
+import { AUTH, type AuthProvider } from '@zentro/constants/auth'
+import { toast } from 'sonner'
+import { envClient } from '@/lib/config/env-client'
 import { authClient } from '@/lib/services/auth-client'
 
 export const signOut = async () => {
   try {
-    const session = await authClient.signOut()
-    return session
+    const res = await authClient.signOut()
+    return res
   } catch {
+    toast.error(AUTH.errors.failedSignOut.message)
     return {
-      data: null,
-      error: {
-        type: HTTP_ERRORS.networkError.type,
-        message: HTTP_ERRORS.networkError.message,
-        statusCode: HTTP_ERRORS.networkError.statusCode,
-      },
+      data: undefined,
+      error: AUTH.errors.failedSignOut,
+    }
+  }
+}
+
+export const deleteAccount = async () => {
+  try {
+    const res = await authClient.deleteUser({
+      callbackURL: '/',
+    })
+    return res
+  } catch {
+    toast.error(AUTH.errors.failedDeleteAccount.message)
+    return {
+      data: undefined,
+      error: AUTH.errors.failedDeleteAccount,
+    }
+  }
+}
+
+export const signInWith = async (provider: AuthProvider) => {
+  try {
+    const res = await authClient.signIn.social({
+      provider,
+      callbackURL: envClient.NEXT_PUBLIC_BASE_URL,
+      errorCallbackURL: envClient.NEXT_PUBLIC_BASE_URL,
+    })
+    return res
+  } catch {
+    toast.error(AUTH.errors.failedSignIn.message)
+    return {
+      data: undefined,
+      error: AUTH.errors.failedSignIn,
+    }
+  }
+}
+
+export const signInAnonymous = async () => {
+  try {
+    const res = await authClient.signIn.anonymous()
+    return res
+  } catch {
+    toast.error(AUTH.errors.failedAnonymousSignIn.message)
+    return {
+      data: undefined,
+      error: AUTH.errors.failedAnonymousSignIn,
     }
   }
 }
